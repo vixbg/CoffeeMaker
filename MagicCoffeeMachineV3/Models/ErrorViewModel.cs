a5ff4a679cc539aa878789bff046f58c5b3724a5
victor.vassilev@sheppatechnologies.com <victor.vassilev@sheppatechnologies.com>
2024-02-06 22:53:47 +0200

    Initial Project Creation MVC

---
+++ MagicCoffeeMachineV3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MagicCoffeeMachineV3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
